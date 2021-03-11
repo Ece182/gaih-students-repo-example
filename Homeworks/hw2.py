2d1a6b77d14d6b2f75c9d9b0e19a3e51dc3d2802
Ece182 <80275580+Ece182@users.noreply.github.com>
2021-03-12 00:03:24 +0300

    Update and rename HW2.py to hw2.py
    globalalhubday2hw

---
+++ Homeworks/hw2.py
@@ -0,0 +1,7 @@
+#CV application
+CV1 = {"name":"Ada", "Surname":"Lovelace", "age": "36", "profession": "Python" }
+CV2 = {"name":"Alan", "surname":"Turing", "age": "42", "profession": "Python" }
+CV3 = {"name":"Charles", "Surname":"Babbage", "age": "80", "profession": "Python" }
+CV4 = {"name":"Mark", "Surname":"Zuckerberg", "age": "36", "profession": "Python" }
+CV5 = {"name":"Elon", "Surname":"Musk", "age": "49", "profession": "Python" }
+print(CV1, CV2, CV3, CV4, CV5. sep=" \n")
