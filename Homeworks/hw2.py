fb440fc5f475cdc425ad0e2d2c07226c07b78ecb
Ece182 <80275580+Ece182@users.noreply.github.com>
2021-03-12 00:03:49 +0300

    Update hw2.py

--- Homeworks/hw2.py
+++ Homeworks/hw2.py
@@ -1,7 +1,7 @@
 #CV application
-CV1 = {"name":"Ada", "Surname":"Lovelace", "age": "36", "profession": "Python" }
-CV2 = {"name":"Alan", "surname":"Turing", "age": "42", "profession": "Python" }
-CV3 = {"name":"Charles", "Surname":"Babbage", "age": "80", "profession": "Python" }
-CV4 = {"name":"Mark", "Surname":"Zuckerberg", "age": "36", "profession": "Python" }
-CV5 = {"name":"Elon", "Surname":"Musk", "age": "49", "profession": "Python" }
+CV1 = {"Name":"Ada", "Surname":"Lovelace", "age": "36", "profession": "Python" }
+CV2 = {"Name":"Alan", "Surname":"Turing", "age": "42", "profession": "Python" }
+CV3 = {"Name":"Charles", "Surname":"Babbage", "age": "80", "profession": "Python" }
+CV4 = {"Name":"Mark", "Surname":"Zuckerberg", "age": "36", "profession": "Python" }
+CV5 = {"Name":"Elon", "Surname":"Musk", "age": "49", "profession": "Python" }
 print(CV1, CV2, CV3, CV4, CV5. sep=" \n")
