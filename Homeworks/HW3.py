09c26b7f75525360a1d55abcb6e38abfb2c58d1d
Ece182 <80275580+Ece182@users.noreply.github.com>
2021-03-12 18:10:00 +0300

    Update HW3.py
    globalhubday4hw

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,32 @@
-#Explain your work
+#Students&Average HW3
 
-#Question 1
-for x in range(a):
-	print(a)
+midterm = int(input('midterm grade: '))
+project = int(input('project grade: '))
+final =  int(input('final grade: '))
+
+
+average = midterm*(0.3) + project*(0.3) + final *(0.4)
+
+#Grading scale
+def determine_score(average):
+    if 90 <= average <= 100:
+        return 'A'
+    elif 80 <= average <= 89:
+        return 'B'
+    elif 70 <= average <= 79:
+        return 'C'
+    else:
+        return 'F'
+
+grade = average
+avg = determine_score(average)
+
+print("That's a(n): " + str(grade))
+print('Average grade is: ' + str(avg))
+
+
+studentavg = ['40','50','69','78','56']
+studentavg.sort(reverse=True)
+print(studentavg)
+
+#ps: I couldn't do the homework correctly I know that, but I did my own best.
