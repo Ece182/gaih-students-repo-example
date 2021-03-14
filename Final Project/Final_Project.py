57333d1ec04a5186c207cde6a57cf8e0ceb7c9d6
Ece182 <80275580+Ece182@users.noreply.github.com>
2021-03-14 21:42:53 +0300

    Update Final_Project.py
    globalaihubfinal

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -1,3 +1,19 @@
-import pandas as pd
+print("Quiz")
 
-data = pd.read_csv("project.csv")
+question = "Who invented the telephone?"
+options = "a.Myslef\nb. My grandpa \nc.Donald Trump \nd. Alexander Graham Bell\n"
+
+print(question)
+print(options)
+
+while True:
+    response = input("Hit 'a', 'b', 'c' or 'd' for your answer\n")
+
+    if response == "d":
+        print("Congrats you earned 10 points.")
+    else:
+        print("Incorrect! Try again.")
+       
+      
+#I couldn't do this homework.Sorry about that. I need more practise. One day I hope I'll do it.
+#Thank you for the python education.
